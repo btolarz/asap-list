f31c226a5f92e23f7b44171e1584c2f78352bd67
btolarz <btolarz@gmail.com>
2010-12-29 10:18:57 +0100

    changed list items sort

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ActiveRecord::Base  
-  has_many :list_items
+  has_many :list_items, :order => 'created_at DESC'
   
   accepts_nested_attributes_for :list_items, :allow_destroy => true
 end
